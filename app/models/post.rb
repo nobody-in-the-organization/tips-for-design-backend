d89646fb8f7e1e3f98352e2a07d9c158c66bc808
Akiicat <aaaa1379@gmail.com>
2018-03-27 19:35:36 +0800

    add post and fix bug

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ApplicationRecord
+  before_create :generate_uid
+
+  acts_as_taggable
+
+  private
+
+  def generate_uid
+    loop do
+      self.uid = SecureRandom.urlsafe_base64(8)
+      break if not Post.find_by(uid: self.uid)
+    end
+  end
+end
